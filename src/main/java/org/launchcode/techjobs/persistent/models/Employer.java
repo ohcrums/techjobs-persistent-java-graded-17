e11a8f14b3bf3d98e787c3b705551177f006a8fa
ohcrums <149456208+ohcrums@users.noreply.github.com>
2024-05-27 13:16:15 -0500

    task-2 complete
    Set up entities, AbstractEntity, and repositories. Set up EmployerController and SkillController

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,23 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String location;
+
+    public Employer() {}
+
+    public @NotBlank @Size(min = 3, max = 50) String getLocation() {
+        return location;
+    }
+
+    public void setLocation(@NotBlank @Size(min = 3, max = 50) String location) {
+        this.location = location;
+    }
 }
